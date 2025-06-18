bd077e9283498558260774a291ff8a8506e31d31
Erickdrg <147166781+Erickdrg@users.noreply.github.com>
2025-06-18 15:15:41 -0600

    conexion.py

--- RegistrarPagoyConciliar/conexion.py
+++ RegistrarPagoyConciliar/conexion.py
@@ -1,4 +1,8 @@
+# conexion.py
 import sqlite3
+from sqlite3 import Connection
 
-def conectar():
-    return sqlite3.connect("Base_datos.db")
+def conectar() -> Connection:
+    conn = sqlite3.connect("Base_datos.db", timeout=10)
+    conn.execute("PRAGMA journal_mode=WAL")  # Mejor manejo de concurrencia
+    return conn
